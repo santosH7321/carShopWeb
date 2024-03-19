06c3036a084bd04f2eea99b19518611c7a0ed201
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-03-19 15:57:16 +0530

    Navbar

--- carapp/components/CustomButton.tsx
+++ carapp/components/CustomButton.tsx
@@ -1,18 +1,28 @@
-"use client"
+"use client";
 
-const CustomButton = () => {
-  return (
-    <button
-    disabled={false}
-    type={"button"}
-    className={`custom-btn`}
-    onClick={() => {}}
-    >
-        <span className={`flex-1`}>
-            title
-        </span>
-    </button>
-  )
-}
+import Image from "next/image";
 
-export default CustomButton
+import { CustomButton } from ".";
+
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
+
+export default Button;
