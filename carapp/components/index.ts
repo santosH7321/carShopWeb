f8a9fca6de4d6616e82c15ba234901dc3c811e4e
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-03-19 16:27:21 +0530

    SearchBar

--- carapp/components/index.ts
+++ carapp/components/index.ts
@@ -2,10 +2,14 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter
 }
